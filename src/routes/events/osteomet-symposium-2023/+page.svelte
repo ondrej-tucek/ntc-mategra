ad48fb8b5a8d6f44f4f464a43ed00c2d03555c4a
Ondrej Tucek <ondrej.tucek@gmail.com>
2023-03-10 11:23:34 +0100

    Add event page

---
+++ src/routes/events/osteomet-symposium-2023/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+  import Icon from "../../../components/Icon.svelte";
+</script>
+
+You are all warmly invited to
+<h2 class="article-title">
+  <div>Innovative surfaces and regeneration of musculoskeletal tissues</div>
+</h2>
+symposium on March 30, 2022.
+
+<div class="py-4">
+  The symposium is established within the project No. 349 OSTEOMET:
+  <div class="py-5 px-5 is-centered is-italic">
+    Augmented treatment of osteoporotic bone with innovative organic-inorganic
+    nanoparticle functionalized metal surfaces.
+  </div>
+  This is a cross-border cooperation program Czech Republic - Free State of Bavaria
+  - EWO goal 2014 - 2020. Our project develops cooperation between NTC (New Technologies
+  - Research Center) and UKR (University Clinic in Regensburg).
+</div>
+
+<div class="pb-5">
+  <img
+    class="image image--resize image-border"
+    src={"/images/2022-OSTEOMET-virtual-symposium-poster.jpg"}
+    alt="Virtual symposium poster"
+    loading="lazy"
+  />
+</div>
+
+<div class="pb-4">
+  See pdf
+  <a href="/pdfs/OSTEOMET_workshop_26.07.2022_poster.pdf" target="_blank"
+    >flyer <Icon i={"pdf"} />
+  </a>
+</div>
+
+<style>
+</style>
